aced86d4a8ed9b5a001fa414f9d7d4e5a18b7590
Saulius Raudys <saulius.raudys@asseco.lt>
2019-02-09 13:02:32 +0200

    sping-boot: rest and mongodb

---
+++ src/main/java/com/gildedrose/gildedrosekata/GildedRoseKataApplication.java
@@ -0,0 +1,14 @@
+package com.gildedrose.gildedrosekata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GildedRoseKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GildedRoseKataApplication.class, args);
+	}
+
+}
+
---
+++ src/main/java/com/gildedrose/gildedrosekata/ItemsController.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.gildedrose.gildedrosekata;
+
+import com.gildedrose.Item;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ *
+ * @author saulius
+ */
+@RestController
+public class ItemsController {
+
+    private static final String template = "Hello, %s!";
+
+    @RequestMapping("/items")
+    public List<Item> greeting() {
+        return new ArrayList<>(Arrays.asList(new Item[]{
+            new Item("+5 Dexterity Vest", 10, 20), //
+            new Item("Aged Brie", 2, 0), //
+            new Item("Elixir of the Mongoose", 5, 7), //
+            new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
+            new Item("Sulfuras, Hand of Ragnaros", -1, 80),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
+            new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
+            new Item("Conjured Mana Cake", 3, 6)}));
+    }
+}
